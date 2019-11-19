0d6da99cad86d029e5c2c534e3160376eb12a9f3
Akamoto529 <40491944+Akamoto529@users.noreply.github.com>
2019-11-20 00:25:51 +0300

    Revert "Начнём с этого"
    This reverts commit 0ac163cc4ddf09f237b03313fd94019868e56a29.

--- GamePrototype/GamePrototype/Main.cpp
+++ GamePrototype/GamePrototype/Main.cpp
@@ -1,11 +1,11 @@
 #include <SFML/Graphics.hpp>
-#include "Player.h"
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 1000), "SFML works!");
-	Projectile pr(500.f,400.f);
-	Player pl;
-	pl.sprite.setPosition(window.getSize().x / 2,window.getSize().y-30.f);
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -14,16 +14,11 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		pl.Shoot();
-		pl.sprite.move(pl.Speed * pl.getDir());
+
 		window.clear();
-		for (int i = 0; i < pl.proj.size(); i++) {
-			pl.proj[i].sprite.move(pl.proj[i].speed*pl.proj[i].dir);
-			//if (pl.proj[i].sprite.getPosition().y < 100.f) pl.proj.erase(pl.proj.begin() + i);
-			window.draw(pl.proj[i].sprite);
-		}
-		window.draw(pl.sprite);
+		window.draw(shape);
 		window.display();
 	}
+
 	return 0;
 }
