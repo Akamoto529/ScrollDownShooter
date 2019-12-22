b5a93bef65a1ed271254ec73c1ba90615217a7cd
Akamoto529 <40491944+Akamoto529@users.noreply.github.com>
2019-12-22 12:05:06 +0300

    Commit из ниоткуда

--- GamePrototype/GamePrototype/loader.h
+++ GamePrototype/GamePrototype/loader.h
@@ -1,19 +1,14 @@
 ﻿#pragma once
 #include "SFML/Graphics.hpp"
-#include <vector>
-#include "config.h"
 #include "Hitbox.h"
-//#include <map>
 
 // Загружает и хранит в себе текстуры и схемы хитбоксов (Singleton).
-// список объектов - см. config.h.
 class Loader
 {
 	static Loader* instance;
 	Loader();
 	std::map<std::string, sf::Texture> Textures;
 	std::map<std::string, Hitbox> Hitboxes;
-	//Hitbox loadHB(std::string Name);
 public:
 	static Loader* get()
 	{
