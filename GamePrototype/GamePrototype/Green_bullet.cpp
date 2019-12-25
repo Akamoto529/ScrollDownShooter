919ca4b003e883cb23ffc95a9de74e06b016bce6
MK <frbeast@outlook.com>
2019-12-25 12:04:11 +0300

    up-to-date

---
+++ GamePrototype/GamePrototype/Green_bullet.cpp
@@ -0,0 +1,1 @@
+#include "Green_bullet.h"
---
+++ GamePrototype/GamePrototype/Green_bullet.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Projectile.h"
+
+class Green_bullet : public Projectile
+{
+public:
+	Green_bullet(const sf::Vector2f pos, const sf::Vector2f direction);
+};
