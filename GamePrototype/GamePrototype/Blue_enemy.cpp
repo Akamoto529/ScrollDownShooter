0e552597afde5e184abe32b983d65742e342c00f
BlackBondar <blackbondar1@gmail.com>
2019-12-25 14:34:01 +0300

    lvl2

--- GamePrototype/GamePrototype/Blue_enemy.cpp
+++ GamePrototype/GamePrototype/Blue_enemy.cpp
@@ -2,7 +2,7 @@
 #include "Blue_gun.h"
 
 Blue_enemy::Blue_enemy(const sf::Vector2f pos)
-	: Enemy(pos, 40.f, "Blue_enemy")
+	: Enemy(pos, 60.f, "Blue_enemy")
 {
 	this->HP = 3;
 	this->gun = new Blue_gun(sf::Vector2f(0, 1));
